775ab310f282fd7534655c5709d15812b18e3c16
pawjam <jamro.pawel@gmail.com>
2022-06-13 19:31:29 +0200

    URL copy

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3_1/console/CopyFromUrl.java
@@ -0,0 +1,40 @@
+package pl.lublin.wsei.java.cwiczenia.lab3_1.console;
+
+import java.io.*;
+import java.net.URL;
+
+public class CopyFromUrl {
+    public static void main(String[] args) throws IOException {
+        BufferedInputStream in=null;
+        BufferedOutputStream out=null;
+        URL url=new URL("https://filesamples.com/samples/image/tiff/sample_1920%C3%971280.tiff");
+        long startTime=System.nanoTime();
+
+        try{
+            in =new BufferedInputStream((url.openStream()));
+            out=new BufferedOutputStream(new FileOutputStream("img_copy_url.tiff"));
+            int c;
+            while((c = in.read()) != -1){
+                out.write(c);
+            }
+        }
+        catch (IOException e){
+            System.out.println("IOexception: "+e.getMessage());
+            e.printStackTrace();
+
+        }
+        finally {
+            if (in != null){
+                in.close();
+            }
+            if(out !=null){
+                out.close();
+            }
+        }
+        long endTime =System.nanoTime();
+        long timeElapsed=endTime-startTime;
+        System.out.println("Czas wykonania w nanosekundach: "+timeElapsed);
+        System.out.println("Czas wykonania w milisekundach: "+timeElapsed/1000000);
+
+    }
+}
--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3_1/console/TestNioCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3_1/console/TestNioCopy.java
@@ -8,12 +8,12 @@ import java.nio.file.StandardCopyOption;
 
 public class TestNioCopy {
     public static void main(String[] args) throws IOException {
-        //long startTime=System.currentTimeMillis();
+        long startTime1=System.currentTimeMillis();
         long startTime=System.nanoTime();
         Path oryginal=Paths.get("sample_1920×1280.tiff");
         Path kopia=Paths.get("img_copy2.tiff");
-        Files.copy(oryginal,kopia, StandardCopyOption.REPLACE_EXISTING);
-        //System.out.println("Czas kopiowanie: "+(System.currentTimeMillis()-startTime+" ms"));
+        Files.copy(oryginal,kopia /*StandardCopyOption.REPLACE_EXISTING*/);
+        System.out.println("Czas kopiowanie: "+(System.currentTimeMillis()-startTime1+" ms"));
         System.out.println("Czas kopiowania: "+(float)(System.nanoTime()-startTime)/1000000+" ms");
 
     }
