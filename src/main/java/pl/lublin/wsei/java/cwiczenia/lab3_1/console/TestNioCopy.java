bc786374681a0e25be2e4195598bb48bb0cf1715
pawjam <jamro.pawel@gmail.com>
2022-06-13 19:12:01 +0200

    NIO copy

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3_1/console/TestNioCopy.java
@@ -0,0 +1,20 @@
+package pl.lublin.wsei.java.cwiczenia.lab3_1.console;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class TestNioCopy {
+    public static void main(String[] args) throws IOException {
+        //long startTime=System.currentTimeMillis();
+        long startTime=System.nanoTime();
+        Path oryginal=Paths.get("sample_1920×1280.tiff");
+        Path kopia=Paths.get("img_copy2.tiff");
+        Files.copy(oryginal,kopia, StandardCopyOption.REPLACE_EXISTING);
+        //System.out.println("Czas kopiowanie: "+(System.currentTimeMillis()-startTime+" ms"));
+        System.out.println("Czas kopiowania: "+(float)(System.nanoTime()-startTime)/1000000+" ms");
+
+    }
+}
